974aeb4eda9ffd2d4d305c109083a1236a4d36db
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-03 11:55:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  post '/reverse' do
-  original_string = params["string"]
-  @reversed_string = original_string.reverse
- 
-  erb :reversed
-end
+  get '/reverse' do
+    erb :reverse
+  end
 
   post '/reverse' do
     erb :reversed
